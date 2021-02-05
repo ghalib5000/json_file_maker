ee5fee77e8760a88f3c55b5f5d9eaa61a98876c5
ghalib5000 <ghalibahmed5000@gmail.com>
2021-02-05 15:54:13 +0500

    second

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace node_telegram_wallpaper_bot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string outputlocation = @"/home/ghalib/images.js";
+            string list = @"/home/ghalib/file.txt";
+            string text;
+            StreamReader fs = new StreamReader(list);
+           
+            while ((text = fs.ReadLine()) != null)
+            {
+                Console.WriteLine(text);
+                File.AppendAllText(outputlocation, "file: '" + text + "',\n");
+
+                File.AppendAllText(outputlocation, "description: '"+text.Substring(0, (text.Length-4))+"' \n");
+                File.AppendAllText(outputlocation, "},\n{\n");
+            }
+            }
+        }
+       
+}
